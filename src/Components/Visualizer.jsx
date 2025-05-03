82db9b1be180f0cdac3c1e5dc983303ece17daf4
pikapower9080 <pikapower9080@gmail.com>
2025-05-03 10:22:42 -0400

    feat: allow cycling presets by clicking

--- src/Components/Visualizer.jsx
+++ src/Components/Visualizer.jsx
@@ -11,6 +11,10 @@ export default function Visualizer(props) {
   const { setLoading } = useContext(LoadingContext);
 
   const canvasRef = useRef(null);
+  const presetIndex = useRef(0);
+  const visualizerRef = useRef(null);
+
+  let presets = useRef(null);
 
   useEffect(() => {
     let closing = false;
@@ -19,6 +23,7 @@ export default function Visualizer(props) {
     async function setup() {
       const butterchurn = (await import("butterchurn")).default;
       const butterchurnPresets = (await import("butterchurn-presets")).default;
+      presets.current = butterchurnPresets.getPresets();
 
       const streamNode = audioContext.createMediaStreamSource(props.audioRef.current.captureStream());
       props.audioRef.current.muted = true;
@@ -34,10 +39,11 @@ export default function Visualizer(props) {
         pixelRatio: window.devicePixelRatio || 1,
         textureRatio: 1
       });
+      visualizerRef.current = visualizer;
 
       visualizer.connectAudio(gainNode);
 
-      visualizer.loadPreset(Object.values(butterchurnPresets.getPresets())[0]);
+      visualizer.loadPreset(Object.values(presets.current)[0]);
 
       setLoading(false);
       function render() {
@@ -62,5 +68,36 @@ export default function Visualizer(props) {
     };
   }, []);
 
-  return <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} style={canvasStyle}></canvas>;
+  return (
+    <canvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      style={canvasStyle}
+      onClick={() => {
+        if (!presets.current || !visualizerRef.current) {
+          return;
+        }
+        if (presetIndex.current >= Object.values(presets.current).length) {
+          presetIndex.current = 0;
+        }
+
+        visualizerRef.current.loadPreset(Object.values(presets.current)[presetIndex.current]);
+        presetIndex.current++;
+      }}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        if (!presets.current || !visualizerRef.current) {
+          return;
+        }
+        if (presetIndex.current <= 0) {
+          presetIndex.current = Object.values(presets.current).length - 1;
+        } else {
+          presetIndex.current--;
+        }
+
+        visualizerRef.current.loadPreset(Object.values(presets.current)[presetIndex.current]);
+      }}
+    ></canvas>
+  );
 }
