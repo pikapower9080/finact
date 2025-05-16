32b440c4e059f45f056856fa3358102586995d8a
pikapower9080 <pikapower9080@gmail.com>
2025-05-16 13:41:59 -0400

    enhancement: send platform to jellyfin on desktop

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, shell } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import Store from "electron-store";
 import path from "path";
 
--- app/preload.js
+++ app/preload.js
@@ -1,6 +1,18 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
+const platformNames = {
+  darwin: "macOS",
+  win32: "Windows",
+  linux: "Linux",
+  freebsd: "FreeBSD",
+  openbsd: "OpenBSD",
+  sunos: "Solaris",
+  android: "Android"
+};
+
 contextBridge.exposeInMainWorld("electron", {
   sendMessage: (message) => ipcRenderer.send("message-from-renderer", message),
-  onMessage: (callback) => ipcRenderer.on("reply-from-main", (event, arg) => callback(arg))
+  onMessage: (callback) => ipcRenderer.on("reply-from-main", (event, arg) => callback(arg)),
+  platform: platformNames[process.platform] || process.platform,
+  isDev: process.env.NODE_ENV === "dev"
 });
